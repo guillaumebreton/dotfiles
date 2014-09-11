85e4ae4a22d5d5b16480cb04aa266564f8b9b143
Guillaume Breton <breton.gy@gmail.com>
2014-09-11 10:57:10 +0200

    Create a bootstrap file to install everything

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sF $DIR/.tmux.conf $HOME/.tmux.conf
+ln -sF $DIR/.vimrc $HOME/.vimrc
+ln -sF $DIR/.zshrc $HOME/.zshrc
+ln -sF $DIR/.gitconfig $HOME/.gitconfig
+ln -sF $DIR/.slate $HOME/.slate
+ln -sF $DIR/.wgetrc $HOME/.wgetrc
+ln -sF $DIR/yak.zsh-theme $HOME/.oh-my-zsh/themes/yak.zsh-theme
