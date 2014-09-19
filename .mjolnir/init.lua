eeaf8ae967e0eeae8a72ea68fd04e1737698e1eb
Guillaume Breton <breton.gy@gmail.com>
2014-09-19 17:26:00 +0200

    Change modifier for shortcuts

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -27,31 +27,31 @@ function throw(win, screen_number)
 end
 
 -- Push window to the left with 50% width
-hotkey.bind({"ctrl","cmd"}, "left", function()
+hotkey.bind({"shift","cmd"}, "left", function()
   moveTo(0,0,0.5,1)
 end)
 
 -- Push focused window to the right with 50% width
-hotkey.bind({"ctrl","cmd"}, "right", function()
+hotkey.bind({"shift","cmd"}, "right", function()
   moveTo(0.5, 0, 0.5, 1)
 end)
 
 -- Push focused window at the top with 50% height
-hotkey.bind({"ctrl","cmd"}, "up", function()
+hotkey.bind({"shift","cmd"}, "up", function()
   moveTo(0, 0, 1, 0.5)
 end)
 
 -- Push focused window at the bottom with 50% height
-hotkey.bind({"ctrl","cmd"}, "down", function()
+hotkey.bind({"shift","cmd"}, "down", function()
   moveTo(0, 0.5, 1, 0.5)
 end)
 
 -- Push window full screen to first screen
-hotkey.bind({"ctrl","cmd"}, "&", function()
+hotkey.bind({"shift","cmd"}, "&", function()
   throw(window.focusedwindow(),1)
 end)
 
 -- Push window full screen to second screen
-hotkey.bind({"ctrl","cmd"}, "é", function()
+hotkey.bind({"shift","cmd"}, "é", function()
   throw(window.focusedwindow(), 2)
 end)
