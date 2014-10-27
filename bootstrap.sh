e1fed11dd9908be0c563c6aca8018ec45d481f44
Guillaume Breton <breton.gy@gmail.com>
2014-10-27 14:34:38 +0100

    Use stow to manage dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,7 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ln -sF $DIR/.tmux.conf $HOME/.tmux.conf
-ln -sF $DIR/.vimrc $HOME/.vimrc
-ln -sF $DIR/.zshrc $HOME/.zshrc
-ln -sF $DIR/.gitconfig $HOME/.gitconfig
-ln -sF $DIR/.slate $HOME/.slate
-ln -sF $DIR/.wgetrc $HOME/.wgetrc
-ln -sF $DIR/yak.zsh-theme $HOME/.oh-my-zsh/themes/yak.zsh-theme
-ln -sF $DIR/.mongorc.js $HOME/.mongorc.js
-ln -sF $DIR/.mjolnir $HOME/.mjolnir
+stow vi
+stow git
+stow zsh
+stow mongodb
+stow tmux
+stow mjolnir
+stow wget
