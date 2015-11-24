504225bc45d45cf16f64145528e2d337c7b1767f
Guillaume Breton <breton.gy@gmail.com>
2015-11-24 15:03:00 +0100

    Fix editor and add alias

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -4,3 +4,9 @@ alias fuck='sudo $(fc -ln -1)'
 alias rm='trash'
 alias m="make"
 alias tf="terraform"
+
+alias -g ...="../.."
+alias -g ....="../../.."
+alias -g .....="../../../.."
+alias -g ......="../../../../../"
+
